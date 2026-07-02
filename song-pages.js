c206667beb5ca1a1fee2679ca4492e9ebd8e98ca
nathanielvaldivieso-art <nathaniel.valdivieso@gmail.com>
2026-07-02 07:08:02 -0400

    Publish song pages from studio

--- song-pages.js
+++ song-pages.js
@@ -5,12 +5,27 @@ window.burnfolderSongPages = {
     "lyrics": "",
     "versions": {
       "7VAAvi4x01KJBoNhJPvuOS5LDx02oN9NUhuDEHK2nPpPI": {
-        "lyrics": "sometimes\n\ni dont know why i stay\n\nbut sometimes\n\nthere aint no other place"
+        "lyrics": "sometimes\n\ni dont know why i stay\n\nbut sometimes\n\nthere aint no other place",
+        "notes": ""
       }
     },
     "heroVideoPlaybackId": "",
     "coverArt": "",
     "media": [],
     "updatedAt": "2026-06-22T17:32:33.592Z"
+  },
+  "rgb": {
+    "notes": "",
+    "lyrics": "",
+    "versions": {
+      "u7dao7pbsgOZGdcVfPv6gFA8Jke3HhaKsoNwtAer013k": {
+        "lyrics": "i cant escape from it anymore\n\nits getting harder to close to door\n\ndont think I want to anymore\n\nand i dont know who i did it for\n\nis there a word for whats innocent\n\nand when it turns into ignorance?\n\ntryna solve for the difference\n\nand maybe not, but i think I get it now\n\n\n\nand i can't help but fantasize\n\nthinking bout if i did it right\n\ni been doing it my whole life\n\nliving out something out of time\n\nand i hope that youre doing fine\n\nI dont know cause its out of my\n\nmy control I been tryna focus on the light\n\nand its been slow but its worth it cause I think I get it now\n\n\n\n\n\n\n\n",
+        "notes": "been working with the push more.. everything on this was played/sequenced"
+      }
+    },
+    "heroVideoPlaybackId": "",
+    "coverArt": "",
+    "media": [],
+    "updatedAt": "2026-07-02T11:07:59.250Z"
   }
 };
